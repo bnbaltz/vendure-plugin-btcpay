d10650aee775f99a1441a9cbc0d05c47d3021b75
Ben Baltz <bnbaltz@gmail.com>
2024-09-25 14:22:03 -0700

    correct compiler options for decorators & req import

--- src/btcpay.controller.ts
+++ src/btcpay.controller.ts
@@ -1,16 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { Resolver, Mutation } from '@nestjs/graphql';
 import { BTCPayService } from './btcpay.service';
 import { Ctx, Logger, RequestContext } from '@vendure/core';
 import { InvoiceConfirmedWebhookEvent } from './btcpay.types';
 import { loggerCtx } from './constants';
+import { Request } from 'express';
 
 @Controller('payments')
 export class BTCPayController {
   constructor(private service: BTCPayService) {}
 
   @Post('btcpay')
-  async webhook(@Body() body: InvoiceConfirmedWebhookEvent, @Req() req): Promise<void> {
+  async webhook(@Body() body: InvoiceConfirmedWebhookEvent, @Req() req: Request): Promise<void> {
     try {
       await this.service.settlePayment(body.event, req);
     } catch (error: any) {
